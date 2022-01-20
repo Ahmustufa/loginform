1bd02d68a3e8b6039c31bff48059621fbefd931d
Ahmustufa <ahmedmustafa1234@gmail.com>
2022-01-20 21:11:38 +0500

    email and password validation on signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,42 +1,63 @@
-import React from "react";
-import { Button, Col, FormControl, InputGroup, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Col, Form, FormControl, InputGroup, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../componentStyles/Login.css";
 const Login = () => {
+  // const initialValues = { username: "", email: "", password: "" };
+  // const [formValues, setFormValues] = useState(initialValues);
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormValues({ ...formValues, [name]: value });
+  //   console.log(formValues);
+  // };
+
+  // const handleSubmit = (e)=> {
+  //   e.preventDefault();
+  // }
   return (
     <div className="login1">
       <Row>
+        {/* <pre>{JSON.stringify(form)}</pre> */}
         <Col md={12} className="border">
           <Row className="border">
             <h1>Login Page</h1>
             <Row>
               <Col md={12}>
-                <InputGroup className="mb-3">
-                  <InputGroup.Text id="inputGroup-sizing-default">
-                    Email
-                  </InputGroup.Text>
-                  <FormControl
-                    aria-label="Default"
-                    aria-describedby="inputGroup-sizing-default"
-                  />
-                </InputGroup>
-                <InputGroup className="mb-3">
-                  <InputGroup.Text id="inputGroup-sizing-default">
-                    Password
-                  </InputGroup.Text>
-                  <FormControl
-                    aria-label="Default"
-                    aria-describedby="inputGroup-sizing-default"
-                  />
-                </InputGroup>
+                <Form>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" placeholder="" />
+                  </Form.Group>
+
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" />
+                  </Form.Group>
+
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="*******"  />
+                  </Form.Group>
+                </Form>
                 <Row>
                   <Col md={10}>
-                  <Button variant="dark">Forgot Your Password?</Button>
-                    <Button variant="dark"><Link to={"/signup"} className="link">Sign up</Link></Button>
+                    <Button variant="dark">Forgot Your Password?</Button>
+                    <Link to={"/signup"} className="link">
+                      <Button variant="dark">Sign up</Button>
+                    </Link>
                   </Col>
 
                   <Col md={2}>
-                  <Button variant="dark">Submit</Button>
+                    <Button variant="dark">Submit</Button>
                   </Col>
                 </Row>
               </Col>
@@ -49,3 +70,5 @@ const Login = () => {
 };
 
 export default Login;
+
+// value={formvalues.username} onChange={handleChange}
