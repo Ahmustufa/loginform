fd914df28c90f8b54a78f371ceca151a4b8348a1
Ahmustufa <ahmedmustafa1234@gmail.com>
2022-01-20 21:11:10 +0500

    email and password validation

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "../componentStyles/Signup.css";
-import { Button, Col, FormControl, InputGroup, Row } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Form,
+  FormControl,
+  InputGroup,
+  Row,
+} from "react-bootstrap";
 import "../componentStyles/Signup.css";
+import { validEmail, validPassword } from "../components/Regex";
+
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailErr, setEmailErr] = useState(false);
+  const [pwdError, setPwdError] = useState(false);
+  const validate = () => {
+    if (!validEmail.test(email)) {
+      setEmailErr(true);
+    }
+    if (!validPassword.test(password)) {
+      setPwdError(true);
+    }
+  };
   return (
     <div className="signup">
       <Row>
@@ -11,69 +32,74 @@ const Signup = () => {
             <h1>Sign up</h1>
           </Row>
           <Row>
-            <Col>
-              <InputGroup className="mb-3">
-                <InputGroup.Text id="inputGroup-sizing-default">
-                 First Name
-                </InputGroup.Text>
-                <FormControl
-                  aria-label="Default"
-                  aria-describedby="inputGroup-sizing-default"
-                />
-              </InputGroup>
-              <InputGroup className="mb-3">
-                <InputGroup.Text id="inputGroup-sizing-default">
-                 Last Name
-                </InputGroup.Text>
-                <FormControl
-                  aria-label="Default"
-                  aria-describedby="inputGroup-sizing-default"
-                />
-              </InputGroup>
-              <InputGroup className="mb-3">
-                <InputGroup.Text id="inputGroup-sizing-default">
-                 Email
-                </InputGroup.Text>
-                <FormControl
-                  aria-label="Default"
-                  aria-describedby="inputGroup-sizing-default"
-                />
-              </InputGroup>
-              <InputGroup className="mb-3">
-                <InputGroup.Text id="inputGroup-sizing-default">
-                 Confirm Email
-                </InputGroup.Text>
-                <FormControl
-                  aria-label="Default"
-                  aria-describedby="inputGroup-sizing-default"
-                />
-              </InputGroup>
-              <InputGroup className="mb-3">
-                <InputGroup.Text id="inputGroup-sizing-default">
-                 Password
-                </InputGroup.Text>
-                <FormControl
-                  aria-label="Default"
-                  aria-describedby="inputGroup-sizing-default"
-                />
-              </InputGroup>
-              <InputGroup className="mb-3">
-                <InputGroup.Text id="inputGroup-sizing-default">
-                 Confirm Password
-                </InputGroup.Text>
-                <FormControl
-                  aria-label="Default"
-                  aria-describedby="inputGroup-sizing-default"
-                />
-              </InputGroup>
-             <Row>
-                 <Col md={9}></Col>
-                 <Col md={2}><Button variant="dark">Submit</Button></Col>
-             </Row>
+            <Col md={12}>
+              <Form>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label className="form">First Name</Form.Label>
+                  <Form.Control type="text" placeholder="" />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label className="form">Last Name</Form.Label>
+                  <Form.Control type="text" placeholder="" />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label className="form">Username</Form.Label>
+                  <Form.Control type="text" placeholder="" />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label className="form">Email</Form.Label>
+                  <Form.Control
+                    type="Email"
+                    placeholder="name@example.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label className="form">Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="*******"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Form.Group>
+
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label className="form"> Confirm Password</Form.Label>
+                  <Form.Control type="password" placeholder="*******" />
+                </Form.Group>
+              </Form>
+              <Row>
+                <Col md={7}></Col>
+                <Col md={3}>
+                  <Button variant="dark" onClick={validate}>Submit</Button>
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Col>
       </Row>
+      {emailErr && <p>Your email is invalid</p>}
+      {pwdError && <p>Your password is invalid</p>}
     </div>
   );
 };
